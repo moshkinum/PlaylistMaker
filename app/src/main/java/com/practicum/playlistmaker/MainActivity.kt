5e460fe44844208f4665737d782249863886aed3
Yury Moshkin <yumoshkin@gmail.com>
2025-03-19 20:42:26 +0300

    Sprint 8, homework

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+        val buttonSearchClickListener: View.OnClickListener =
+            object : View.OnClickListener {
+                override fun onClick(p0: View?) {
+                    val searchIntent =
+                        Intent(this@MainActivity, SearchActivity::class.java)
+                    startActivity(searchIntent)
+                }
+            }
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        val buttonLibrary = findViewById<Button>(R.id.library)
+        buttonLibrary.setOnClickListener {
+            val libraryIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(libraryIntent)
+        }
+
+        val buttonSettings = findViewById<Button>(R.id.settings)
+        buttonSettings.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
