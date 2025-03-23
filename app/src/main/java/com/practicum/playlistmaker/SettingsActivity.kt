610dba1a6fb61a208a56a267401cf8e9f9ff6fd6
Yury Moshkin <yumoshkin@gmail.com>
2025-03-23 08:13:10 +0300

    Sprint 8, homework fixed

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,18 +1,17 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val iconBack = findViewById<ImageView>(R.id.icon_back)
-        iconBack.setOnClickListener {
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        toolbar.setNavigationOnClickListener {
             finish()
         }
     }
-
 }
