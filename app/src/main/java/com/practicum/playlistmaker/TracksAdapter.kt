b84fc0192ce9e6aba627de2a24bf79e89c6684c1
Yury Moshkin <yumoshkin@gmail.com>
2025-04-17 17:36:26 +0300

    Sprint 10, homework

---
+++ app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
@@ -0,0 +1,34 @@
+package com.practicum.adapter_lesson_3
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.Track
+import com.practicum.playlistmaker.TracksViewHolder
+
+
+class TracksAdapter(private val tracks: List<Track>) :
+    RecyclerView.Adapter<TracksViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): TracksViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.tracks_view, parent, false)
+        return TracksViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: TracksViewHolder,
+        position: Int,
+    ) {
+        holder.bind(tracks[position])
+    }
+
+    override fun getItemCount(): Int {
+        return tracks.size
+    }
+
+}
